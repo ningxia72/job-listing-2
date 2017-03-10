c1b57246f67cf35ad727f2a6f47bb6c2d25894a0
ningxia72 <ningxia72@gmail.com>
2017-03-10 21:26:03 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
-  root 'jobs#index'
+  root 'welcome#index'
   namespace :admin do
     resources :jobs do
       resources :resumes
       member do
         post :publish
         post :hide
-      end    
+      end
     end
   end
 end
